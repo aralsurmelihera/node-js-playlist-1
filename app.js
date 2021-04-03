b0c65ab237a783e283b93f5a03562407235539f5
aralsurmelihera <79857907+aralsurmelihera@users.noreply.github.com>
2021-04-04 00:46:03 +0300

    controller
    added controller

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var todoController = require('./controllers/todoController')
+
+var app = express()
+
+// set up template engine 
+app.set('view engine', 'ejs')
+
+//static files
+app.use('/assets', express.static('./public'))
+
+// listen to port
+app.listen(3000)
+console.log('you are listening to port 3000')
+
+
+// fire controllers
+todoController(app)
